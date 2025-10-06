a268678bea5131b01d661354cca1aed59131065f
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-06 11:34:36 -0700

    186 More Mouse Events

--- examples/main.py
+++ examples/main.py
@@ -19,12 +19,19 @@ def home():
               d = d_output,
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
               "text-blue-600 hover:bg-red-500 hover:text-white")
-    jp.Div(a=div2, text="I am a cool interactive div!")
+    jp.Div(a=div2, text="I am a cool interactive div!", mouseenter=mouse_enter,
+           mouseleave=mouse_leave,
+           classes = "hover:bg-red-500")
     return wp
 
 def sum_up(widget, msg):
     sum = float(widget.in1.value) + float(widget.in2.value)
     widget.d.text = sum
 
+def mouse_enter(widget, msg):
+    widget.text = "A mouse entered the house!"
+
+def mouse_leave(widget, msg):
+    widget.text = "The mouse left!"
 
 jp.justpy()
