ddde7a46c7ddfcbfc74ed034bbd2e42b57be058a
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-06 10:31:25 -0700

    183 Adding More Components to the Webpage

--- examples/main.py
+++ examples/main.py
@@ -3,18 +3,19 @@ import justpy as jp
 @jp.SetRoute("/home")
 def home():
     wp = jp.WebPage()
-    jp.Div(a=wp, text="Hello World!",
-           classes="text-green-500 bg-yellow-500 "
-                   "font-serif text-lg")
-    jp.Div(a=wp, text="Hello again!")
+    div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+    jp.Input(a=div, placeholder="Enter First Value",
+             classes="form-input")
+    jp.Input(a=div, placeholder="Enter Second Value",
+             classes="form-input")
+    jp.Div(a=div, text="Result goes here...",
+           classes="text-gray-600")
+    jp.Button(a=div, text="Calculate",
+              classes="border border-blue-500 m-2 py-1 px-4 rounded "
+              "text-blue-600 hover:bg-red-500 hover:text-white")
     return wp
 
-@jp.SetRoute("/about")
-def about():
-    wp = jp.WebPage()
-    jp.Div(a=wp, text="Hi World!")
-    jp.Div(a=wp, text="Hi again!")
-    return wp
+
 
 #jp.Route("/home", home)
 
