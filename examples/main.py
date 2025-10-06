f0e24cfe4fec0f3484d18a518e8378ec84c7b261
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-06 10:40:11 -0700

    184 Putting Components in a Grid

--- examples/main.py
+++ examples/main.py
@@ -4,15 +4,21 @@ import justpy as jp
 def home():
     wp = jp.WebPage()
     div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-    jp.Input(a=div, placeholder="Enter First Value",
+    div1 = jp.Div(a=div, classes="grid grid-cols-3 gap-4 p-4")
+
+    jp.Input(a=div1, placeholder="Enter First Value",
              classes="form-input")
-    jp.Input(a=div, placeholder="Enter Second Value",
+    jp.Input(a=div1, placeholder="Enter Second Value",
              classes="form-input")
-    jp.Div(a=div, text="Result goes here...",
-           classes="text-gray-600")
-    jp.Button(a=div, text="Calculate",
+    jp.Div(a=div1, text="Result goes here...", classes="text-gray-600")
+    jp.Div(a=div1, text="Just another div...", classes="text-gray-600")
+    jp.Div(a=div1, text="Yet another div...", classes="text-gray-600")
+
+    div2 = jp.Div(a=div, classes="grid grid-cols-s gap-4")
+    jp.Button(a=div2, text="Calculate",
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
               "text-blue-600 hover:bg-red-500 hover:text-white")
+    jp.Div(a=div2, text="I am a cool interactive div!")
     return wp
 
 
