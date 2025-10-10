1f6e4dc9e1313fd936a824ce6eba36797cbfb0e2
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-10 12:20:59 -0700

    189 Making the About page

---
+++ webapp/about.py
@@ -0,0 +1,19 @@
+import justpy as jp
+
+class About:
+    path = "/about"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-grey-200 h-screen")
+        jp.Div(a=div, text="This is the About page!", classes="text-4xl m-2")
+        jp.Div(a=div, text="""
+        The "About" page for Amazon refers to the company's online presence, which is the world's largest online retailer and a technology company known for services like Amazon Prime and Alexa. Alternatively, "The Amazon" can refer to the Amazon rainforest, the world's largest tropical rainforest, which is home to immense biodiversity and plays a crucial role in regulating the global climate, as detailed on sites like the World Wildlife Fund (WWF) and The Nature Conservancy. """,
+               classes="text-lg")
+        return wp
+
+
+jp.Route(About.path, About.serve)
+
+jp.justpy(port=8001)
+
