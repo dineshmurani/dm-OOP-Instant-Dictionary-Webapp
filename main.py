6255e8ee77232921c1f25a39d86976f453fe098a
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-17 12:24:32 -0700

    202 Making the Routes Automatic

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from webapp.about import About
 from webapp.dictionary import Dictionary
 from webapp.home import Home
 
-jp.Route(Home.path, Home.serve)
-jp.Route(About.path, About.serve)
-jp.Route(Dictionary.path, Dictionary.serve)
+imports = list(globals().values())
+
+# to iterate over imports dictionary:
+for obj in imports:
+    if hasattr(obj, 'path'):
+        jp.Route(obj.path, obj.serve)
+
+# jp.Route(Home.path, Home.serve)
+# jp.Route(About.path, About.serve)
+# jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy(port=8001)
